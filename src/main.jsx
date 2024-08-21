e92ce676e28198427c1004058e0706bcd4560320
Olexandrd <olexandrd@gmail.com>
2024-08-21 21:20:28 +0300

    Remove github pages :(

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_APP_BASENAME}>
+    <BrowserRouter>
       <Toaster />
       <App />
     </BrowserRouter>
